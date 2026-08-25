910b39dbc4cf4d3a9bde9c7e39ba2991c7ea2c45
haraldpeetz-ops <harald.peetz@gmail.com>
2026-08-25 23:41:17 +0200

    R001.6: add ALS integration view code-behind

---
+++ src/Partcounter.App/Views/AlsIntegrationView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace Partcounter.Views;
+
+public partial class AlsIntegrationView : UserControl
+{
+    public AlsIntegrationView()
+    {
+        InitializeComponent();
+    }
+}
