5ab7ba997b0f658959246182ebbac3b3799bf25a
haraldpeetz-ops <harald.peetz@gmail.com>
2026-08-26 22:15:40 +0200

    R001.9: add commissioning view code-behind

---
+++ src/Partcounter.App/Views/CommissioningView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace Partcounter.Views;
+
+public partial class CommissioningView : UserControl
+{
+    public CommissioningView()
+    {
+        InitializeComponent();
+    }
+}
