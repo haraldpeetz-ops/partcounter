19899c39c7dec5511ebed5445f2f1804d7c31a72
haraldpeetz-ops <harald.peetz@gmail.com>
2026-08-28 21:29:52 +0200

    R001.16: add live commissioning view code-behind

---
+++ src/Partcounter.App/Views/LiveCommissioningView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace Partcounter.Views;
+
+public partial class LiveCommissioningView : UserControl
+{
+    public LiveCommissioningView()
+    {
+        InitializeComponent();
+    }
+}
