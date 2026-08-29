6748e80f85dfffd2eaad24d44e126f9398ffd38d
haraldpeetz-ops <harald.peetz@gmail.com>
2026-08-29 23:41:12 +0200

    R001.23: add proALPHA integration view code-behind

---
+++ src/Partcounter.App/Views/ProAlphaIntegrationView.xaml.cs
@@ -0,0 +1,8 @@
+using System.Windows.Controls;
+
+namespace Partcounter.Views;
+
+public partial class ProAlphaIntegrationView : UserControl
+{
+    public ProAlphaIntegrationView() => InitializeComponent();
+}
